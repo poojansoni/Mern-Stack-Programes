8fcf21511f2e09cdf1fa6f8d005504c1b69a0efa
Poojan <poojansoni.udr@gmail.com>
2021-06-01 23:09:21 +0530

    Added encryption in password Virtual field and methods - Auth,Encrypt

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
+const uuidv1 = require("uuid/v1");
+
 var Schema  = mongoose.Schema;
 
 var userSchema = new Schema({
@@ -27,10 +30,9 @@ var userSchema = new Schema({
         trim: true
     },
 
-    //TODO: come back here
-    password:{
+    encry_password:{
         type: String,
-        trim: true
+        required: true
     },
 
     salt: String,
@@ -45,5 +47,31 @@ var userSchema = new Schema({
         default: []
     }
 });
+
+//dont use arrow functions, not supported much by schema
+userSchema.virtual("password")
+    .set(function(password){
+        this._password = password; //store original pass in private obj
+        this.salt = uuidv1();
+        this.encry_password = this.securePassword(password);
+    }) 
+    .get(function(){
+        return this._password;
+    });
+
+userSchema.method = {
+    authenticate:function(plainPass){
+        return this.securePassword(plainPass) === this.encry_password
+    },
+
+    securePassword = function(plainPass){
+        if(!plainPass) return ""; //returning empty strng will throw err with db cause field is required!
+        try{
+            return crypto.createHmac('sha256',this.salt).update(plainPass).digest('hex'); //storing hashed value direct in db
+        }catch(error){
+            return "";
+        }
+    }
+};
 
 module.exports = mongoose.model("User", userSchema); //maps the sshema to like.. make a class User thru which new user instances can be created
