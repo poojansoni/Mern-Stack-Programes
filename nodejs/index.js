7a49b870c78e4cea966ffec8b46f5b1376af0d91
Poojan <poojansoni.udr@gmail.com>
2021-06-01 17:05:34 +0530

    Trying basic js in node

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,3 +2,15 @@ console.log("Hello Universe");
 const a = 25;
 const b = 9.9;
 console.log(`a = ${a}\nb = ${b}\nTotal =`,a+b);
+
+var add = (...arr) => 
+{
+    sum = 0;
+    arr.forEach(element => 
+    {
+        sum += element;
+    });
+    console.log(sum);
+}
+
+add(1,2,3,5,6,7,8,9,10)
