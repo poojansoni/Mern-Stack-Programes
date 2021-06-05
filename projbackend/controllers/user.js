36233ca47bb6a4dda011ed804c844ddebd0f289f
Poojan <poojansoni.udr@gmail.com>
2021-06-05 23:40:11 +0530

    Update Profile created

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -22,11 +22,32 @@ exports.getUser = (req,res) => {
 
 
 
-exports.getAllUsers = (req,res) => {
+// exports.getAllUsers = (req,res) => {
     
-    User.collection.find().toArray(function(err, result) {
-        if (err) return res.status(400).json({error: err});
-        return res.json(result);
-    });
+//     User.collection.find().toArray(function(err, result) {
+//         if (err) return res.status(400).json({error: err});
+//         return res.json(result);
+//     });
     
+// }
+
+exports.updateUser = (req,res) => {
+    User.findByIdAndUpdate(
+        {_id: req.profile._id}, //1st param id
+        { $set: req.body }, //chnges you want to make
+        { new: true, useFindAndModify: false}, //compulsory parameters
+        (err, user)=>{  //callback
+            if(err){
+                return res.status(400).json({
+                    error: "Could not update profle in DB"
+                });
+            }
+
+            user.salt = undefined;
+            user.encry_password = undefined;
+            user.createdAt = undefined;
+            user.updatedAt = undefined;
+            res.json(user);
+        }
+    );
 }
--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
                            //, getAllUsers    
-const { getUserById, getUser                } = require("../controllers/user");
+const { getUserById, getUser               ,updateUser } = require("../controllers/user");
 const { isSignedIn, isAdmin, isAuthenticated } = require("../controllers/auth");
 
 router.param('userId', getUserById);
-router.get('/user/:userId', isSignedIn, isAuthenticated, getUser)
 
+router.get('/user/:userId', isSignedIn, isAuthenticated, getUser)
+router.put('/user/:userId',isSignedIn, isAuthenticated, updateUser)
 // router.get('/users', getAllUsers)
 
 module.exports = router;
