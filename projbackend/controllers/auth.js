c0174a8081d69d9c62600aecafd9e9defc9e4c8a
Poojan <poojansoni.udr@gmail.com>
2021-06-05 22:36:27 +0530

    GetUser Updated

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -84,7 +84,7 @@ exports.isSignedIn = expressJwt({
 //custom middlewares
 exports.isAuthenticated = (req, res, next) => {
     //from ui properties of user does the profile exists? is the user signed in? and Authenticate -> can chng only his properties so, auth id and profile id should match
-    let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+    let checker = req.profile && req.auth && req.profile._id == req.auth._id;
     if(!checker){
         return res.status(403).json({
             error: "ACCESS DENIED"
