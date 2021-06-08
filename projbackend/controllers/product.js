5f3f3ac5f342b718ba2a472852caa60ce8909f8b
Poojan <poojansoni.udr@gmail.com>
2021-06-08 22:55:22 +0530

    Create product + get product, get photo

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -1,4 +1,7 @@
 const Product = require('../models/product');
+const formidable = require('formidable'); //to collect data like form.. pdfs/imgs etc
+const _ = require('lodash'); //helps with arrays functions
+const fs = require('fs');
 
 exports.getProductById = (req, res, next, id) =>{
     Product.findById(id)
@@ -13,5 +16,59 @@ exports.getProductById = (req, res, next, id) =>{
 };
 
 exports.getProduct = (req, res) =>{
+    req.product.photo = undefined;
     return res.json(req.product);   
+};
+
+//middleware
+exports.photo = (req,res,next)=>{
+    //if data present then onky ret
+    if(req.product.photo.data) 
+    {
+        res.set("Content-Type",req.product.photo.contentType);
+        return res.send(req.product.photo.data);
+    }
+    next();
+}
+
+exports.createProduct = (req,res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err,fields, file) => {
+        if(err){
+            return res.status(400).json({error: "Problem with image"});
+        }
+
+        //destructuring fields
+        const {name,description,price,category,stock} = fields;
+        if(
+            !name ||
+            !description ||
+            !price ||
+            !category ||
+            !stock 
+        ){
+            return res.status(400).json({error: "Please fill all the fields"});
+        }
+        
+        let product = new Product(fields);
+
+        //handle file here
+        if(file.photo){
+            if(file.photo.size>3000000) //3mb
+            {
+                return res.status(400).json({error: "File size too big!"});
+            }
+            product.photo.data = fs.readFileSync(file.photo.path);
+            product.photo.contentType = file.photo.type;
+        }
+
+        //save in db
+        product.save((err, pr) => {
+            if(err){
+                return res.status(400).json({error: "Saving tshirt in Db failed"});
+            }
+            res.json(pr);
+        });
+    });
 };
--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProductById , createProduct, getProduct, getAllProducts, updateProduct, removeProduct} = require("../controllers/product");
+const { getProductById , createProduct, getProduct,photo, getAllProducts, updateProduct, removeProduct} = require("../controllers/product");
 const { isSignedIn, isAdmin, isAuthenticated } = require("../controllers/auth"); //admin can add chng category
 const { getUserById } = require("../controllers/user");
 
@@ -10,8 +10,9 @@ router.param('userId', getUserById);
 router.param('productId', getProductById);
 
 //routes
-//router.post('/product/create/:productId',isSignedIn, isAuthenticated, isAdmin, createProduct);
+router.post('/product/create/:userId',isSignedIn, isAuthenticated, isAdmin, createProduct);
 router.get('/product/:productId', getProduct);
+router.get('/product/photo/:productId', photo);
 //router.get('/products', getAllProducts);
 //router.put('/product/:productId/:userId',isSignedIn, isAuthenticated, isAdmin, updateProduct);
 //router.delete('/product/:productId/:userId',isSignedIn, isAuthenticated, isAdmin, removeProduct);
