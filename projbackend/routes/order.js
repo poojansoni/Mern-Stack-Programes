e2755c371750ec5c24bbd334c244c90315041e86
Poojan <poojansoni.udr@gmail.com>
2021-06-09 17:47:13 +0530

    Update and get status of Order

--- projbackend/routes/order.js
+++ projbackend/routes/order.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { isSignedIn, isAdmin, isAuthenticated } = require("../controllers/auth"); //admin can add chng category
 const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
 const {updateStock } = require("../controllers/product");
-const { getOrderById, createOrder , getAllOrders } = require("../controllers/order");
+const { getOrderById, createOrder , getAllOrders, updateStatus, getOrderStatus } = require("../controllers/order");
 
 
 //params
@@ -14,12 +14,30 @@ router.param('orderId', getOrderById);
 
 //Routes
 router.post('/order/create/:userId',
- isSignedIn, 
- isAuthenticated,
- pushOrderInPurchaseList,
- updateStock, 
- createOrder);
+    isSignedIn, 
+    isAuthenticated,
+    pushOrderInPurchaseList,
+    updateStock, 
+    createOrder);
 
- router.get("/order/all/:userId", isSignedIn, isAuthenticated, isAdmin, getAllOrders)
+router.get("/order/all/:userId",
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    getAllOrders);
+
+//order-status routes
+router.get("/order/status/:userId",
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    getOrderStatus);
+
+router.put('/order/:orderId/status/:userId',
+    isSignedIn, 
+    isAuthenticated,
+    isAdmin,
+    updateStatus);
+
 
 module.exports = router;
