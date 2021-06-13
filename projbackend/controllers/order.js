fe6defa62b1755ed769a4ae13932980fd5764726
Poojan <poojansoni.udr@gmail.com>
2021-06-13 23:28:52 +0530

    some chnges

--- projbackend/controllers/order.js
+++ projbackend/controllers/order.js
@@ -1,52 +1,52 @@
-const {Order,ProductItem} = require('../models/order');
+const { Order, ProductItem } = require("../models/order");
 
-exports.getOrderById = (req, res, next, id) =>{
-    Order.findById(id)
-    .populate('products.product','name price')
-    .exec((err,order)=>{
-        if(err){
-            return res.status(400).json({error: "Order not Found in DB"})
-        }
-        req.order = order;
-        next();
-    });
+exports.getOrderById = (req, res, next, id) => {
+	Order.findById(id)
+		.populate("products.product", "name price")
+		.exec((err, order) => {
+			if (err) {
+				return res.status(400).json({ error: "Order not Found in DB" });
+			}
+			req.order = order;
+			next();
+		});
 };
 
-exports.createOrder = (req,res) => {
-    req.body.order.user = req.profile;
-    const order = new Order(req.body.order);
-    order.save((err,order)=>{
-        if(err){
-            return res.status(400).json({error: "Failed to save your order in DB"});
-        }
-        res.json(order);
-    });
+exports.createOrder = (req, res) => {
+	req.body.order.user = req.profile;
+	const order = new Order(req.body.order);
+	order.save((err, order) => {
+		if (err) {
+			return res.status(400).json({ error: "Failed to save your order in DB" });
+		}
+		res.json(order);
+	});
 };
 
 exports.getAllOrders = (req, res) => {
-    Order.find()
-    .populate("user","_id name")
-    .exec((err, orders)=>{
-        if(err){
-            return res.status(400).json({error: "no Order found"});
-        }
-        res.json(orders);
-    });
+	Order.find()
+		.populate("user", "_id name")
+		.exec((err, orders) => {
+			if (err) {
+				return res.status(400).json({ error: "no Order found" });
+			}
+			res.json(orders);
+		});
 };
 
-exports.getOrderStatus = (req, res) =>{
-    res.json(Order.schema.path("status").enumValues);
+exports.getOrderStatus = (req, res) => {
+	res.json(Order.schema.path("status").enumValues);
 };
 
 exports.updateStatus = (req, res) => {
-    Order.updateOne(
-        {_id: req.body.orderId},
-        {$set: {status: req.body.status}},
-        (err, order) =>{
-            if(err){
-                return res.status(400).json({error: "Cannot update order-status"});
-            }
-            res.json(order);
-        }
-        );
+	Order.updateOne(
+		{ _id: req.body.orderId },
+		{ $set: { status: req.body.status } },
+		(err, order) => {
+			if (err) {
+				return res.status(400).json({ error: "Cannot update order-status" });
+			}
+			res.json(order);
+		},
+	);
 };
