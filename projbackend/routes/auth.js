465f3cf2a34eb26ee85494bc2ab09fb0e62d900b
Poojan <poojansoni.udr@gmail.com>
2021-06-16 14:51:38 +0530

    some chnges

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,23 +1,39 @@
-var express = require('express');
-const { check } = require('express-validator');
+var express = require("express");
+const { check } = require("express-validator");
 var router = express.Router();
 
 //using controller
-var {signout, signup, signin, isSignedIn} = require("../controllers/auth");
+var { signout, signup, signin, isSignedIn } = require("../controllers/auth");
 
 //setting route
 router.get("/signout", signout);
 
-router.post("/signup",[ //setting Validations for fields that are passed
-    check("name").isLength({min:3}).withMessage("Name should be atleast 3 chars"),
-    check("email").isEmail().withMessage("Email is required/Enter Valid email"),
-    check("password").isLength({min:5}).withMessage("Password should be atleast 5 chars")
-],signup)
+router.post(
+	"/signup",
+	[
+		//setting Validations for fields that are passed
+		check("name")
+			.isLength({ min: 3 })
+			.withMessage("Name should be atleast 3 chars"),
+		check("email").isEmail().withMessage("Email is required/Enter Valid email"),
+		check("password")
+			.isLength({ min: 5 })
+			.withMessage("Password should be atleast 5 chars"),
+	],
+	signup,
+);
 
-router.post("/signin",[ //setting Validations for fields that are passed
-    check("email").isEmail().withMessage("Email is required/Enter Valid email"),
-    check("password").isLength({min:5}).withMessage("Password Required (*5)")
-],signin)
+router.post(
+	"/signin",
+	[
+		//setting Validations for fields that are passed
+		check("email").isEmail().withMessage("Email is required/Enter Valid email"),
+		check("password")
+			.isLength({ min: 5 })
+			.withMessage("Password Required (*5)"),
+	],
+	signin,
+);
 
 // router.get("/testroute", isSignedIn,(req,res)=>{
 //     res.json(req.auth);
